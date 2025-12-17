f635d1348823a5b75938423478025a03a8b0d8d5
rodrigo14052005 <rodrigocolaco31@gmail.com>
2025-12-17 19:48:10 +0000

    Teste

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Loja Checkpoint',
-  tagline: 'O site',
+  tagline: 'O site.com',
   favicon: 'img/favicon.ico',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future

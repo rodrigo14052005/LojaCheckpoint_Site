0f204925b49a141bcd1604829aa4bc89b3673cda
rodrigo14052005 <rodrigocolaco31@gmail.com>
2025-12-24 09:55:45 +0000

    Adicionado a imagem do site.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,7 +12,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 const config = {
   title: 'Loja Checkpoint',
   tagline: 'O site.com',
-  favicon: 'img/favicon.ico',
+  favicon: 'img/loja_checkpoint_image.png',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
